071017b710f065c9024e16ede2399bbb55fe6225
sterstun <152955279+sterstun@users.noreply.github.com>
2025-12-14 21:20:19 +0800

    Create index_iterator.h

---
+++ project2/index_iterator.h
@@ -0,0 +1,59 @@
+//===----------------------------------------------------------------------===//
+//
+//                         CMU-DB Project (15-445/645)
+//                         ***DO NO SHARE PUBLICLY***
+//
+// Identification: src/include/index/index_iterator.h
+//
+// Copyright (c) 2018, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+/**
+ * index_iterator.h
+ * For range scan of b+ tree
+ */
+#pragma once
+#include "buffer/buffer_pool_manager.h"
+#include "storage/page/b_plus_tree_leaf_page.h"
+
+namespace bustub {
+
+#define INDEXITERATOR_TYPE IndexIterator<KeyType, ValueType, KeyComparator>
+
+INDEX_TEMPLATE_ARGUMENTS
+class IndexIterator {
+  using LeafPage = BPlusTreeLeafPage<KeyType, ValueType, KeyComparator>;
+
+ public:
+  // you may define your own constructor based on your member variables
+  IndexIterator();
+  IndexIterator(LeafPage *leaf, int index, BufferPoolManager *buffer_pool_manager);
+  ~IndexIterator();  // NOLINT
+
+  // Disable copy operations to prevent double unpin
+  IndexIterator(const IndexIterator &) = delete;
+  auto operator=(const IndexIterator &) -> IndexIterator & = delete;
+
+  // Enable move operations
+  IndexIterator(IndexIterator &&other) noexcept;
+  auto operator=(IndexIterator &&other) noexcept -> IndexIterator &;
+
+  auto IsEnd() -> bool;
+
+  auto operator*() -> const MappingType &;
+
+  auto operator++() -> IndexIterator &;
+
+  auto operator==(const IndexIterator &itr) const -> bool;
+
+  auto operator!=(const IndexIterator &itr) const -> bool;
+
+ private:
+  // add your own private member variables here
+  page_id_t page_id_;
+  LeafPage *leaf_;
+  int index_;
+  BufferPoolManager *buffer_pool_manager_;
+};
+
+}  // namespace bustub
